e5f2d5a0f38344e762b326c0e041b1ef0c58d46e
kodera2t <kodera2t@yahoo.co.jp>
2014-02-15 22:54:22 +0900

    Update dsp_radio_KT0915_2.ino

--- dsp_radio_KT0915_2.ino
+++ dsp_radio_KT0915_2.ino
@@ -2,7 +2,7 @@
  * DSP Radio (KT0915) control program on Arduino IDE
  *
  *  Created on: Feb. 5 , 2014
- *      Author: kodera2t
+ *      Author: kodera2tもへある
  */
 /* Different version: Try if the previous one does not work well*/
 
